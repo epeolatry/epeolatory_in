d5b877643ed550e94e5ed665bfca82904ecd7670
Rohithzr <Rohithzr@users.noreply.github.com>
2021-04-24 19:47:19 +0000

    jekyll build from Action 0b25a9c3ceedac5ae616569b4d61b11d4962b322

---
+++ script/push.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+#
+#}
+commit_website_files() {
+  git clone https://epeolatry:${github_token}@github.com/epeolatry/epeolatory_in.git dist
+  cd dist
+  git checkout gh-pages
+  cp -R ../_site/* ./
+  rm -rf ../_site
+  rm -rf _site
+  git add .
+  git commit -a -m "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+#
+upload_files() {
+  git push --quiet --set-upstream origin gh-pages
+}
+#
+if [ "$TRAVIS_PULL_REQUEST" = "false" ];
+then
+	setup_git
+	commit_website_files
+	upload_files	
+else
+	echo "Will push after merge"
+fi	
